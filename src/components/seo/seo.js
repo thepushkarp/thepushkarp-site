cb31d8a1a9cbc50bab91bb1c594dbffa61ce11d5
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2023-08-20 16:51:21 +0530

    Improve seo and add manifest

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -30,9 +30,41 @@ const Seo = ({ description, title, children }) => {
     <>
       <title>{`${defaultTitle}`}</title>
       <meta name="description" content={metaDescription} />
+      <meta name="author" content="Pushkar Patel" />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="GPTBot" content="index, follow" />
+      <meta name="google" content="notranslate" />
+      <meta name="referrer" content="no-referrer-when-downgrade" />
+      <meta name="referrer" content="strict-origin-when-cross-origin" />
+      <meta name="referrer" content="origin-when-cross-origin" />
+      <meta name="language" content="English" />
+      <meta name="coverage" content="Worldwide" />
+      <meta name="rating" content="General" />
+      <meta name="apple-mobile-web-app-title" content="Pushkar Patel" />
+      <meta name="application-name" content="Pushkar Patel" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta
+        name="msapplication-TileImage"
+        content="../../images/profile-pic.png"
+      />
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: light)"
+        content="#ffffff"
+      />
+
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
+      <meta property="og:url" content="https://thepushkarp.com" />
       <meta property="og:type" content="website" />
+      <meta property="og:locale" content="en_IN" />
+      <meta property="og:image" content="../../images/profile-pic.png" />
+      <meta property="og:image:alt" content="Pushkar Patel" />
+      <meta property="og:image:width" content="256" />
+      <meta property="og:image:height" content="256" />
+      <meta property="og:site_name" content="Pushkar Patel" />
+
       <meta name="twitter:card" content="summary" />
       <meta
         name="twitter:creator"
@@ -40,10 +72,13 @@ const Seo = ({ description, title, children }) => {
       />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content="../../images/profile-pic.png" />
+      <meta name="twitter:image:alt" content="Pushkar Patel" />
+      <meta name="twitter:image:width" content="256" />
+      <meta name="twitter:image:height" content="256" />
       <meta
-        name="theme-color"
-        media="(prefers-color-scheme: light)"
-        content="#ffffff"
+        name="twitter:site"
+        content={site.siteMetadata?.social?.twitter || ''}
       />
       {children}
     </>
