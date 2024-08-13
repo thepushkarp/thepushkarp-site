7a3deb4aa579c7e1e20aa54dd706822ade1870f7
Pushkar Patel <git@thepushkarp.com>
2024-08-13 16:13:32 +0530

    feat: Add conditional rendering for empty blog posts list
    - Added a check for the number of blog posts to conditionally render the list or a message when no posts are available.
    - Introduced `numPosts` variable to store the count of blog posts for easier conditional checks.

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -3,6 +3,7 @@ import { formatDate, getBlogPosts } from '@/app/blog/utils';
 
 export function BlogPosts() {
   let allBlogs = getBlogPosts();
+  let numPosts = allBlogs.length;
 
   // Group posts by year
   const postsByYear = allBlogs.reduce(
@@ -22,23 +23,27 @@ export function BlogPosts() {
 
   return (
     <div>
-      {sortedYears.map(year => (
-        <div key={year}>
-          <h2 className="mb-2 text-sm">{year}</h2>
-          {postsByYear[Number(year)]
-            .sort((a, b) => (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt) ? -1 : 1))
-            .map(post => (
-              <Link key={post.slug} className="flex flex-col space-y-1 mb-4" href={`/blog/${post.slug}`}>
-                <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                  <p className="text-muted-foreground w-[100px] tabular-nums">
-                    {formatDate(post.metadata.publishedAt, false)}
-                  </p>
-                  <p className="tracking-tight">{post.metadata.title}</p>
-                </div>
-              </Link>
-            ))}
-        </div>
-      ))}
+      {numPosts > 0 &&
+        sortedYears.map(year => (
+          <div key={year}>
+            <h2 className="mb-2 text-sm">{year}</h2>
+            {postsByYear[Number(year)]
+              .sort((a, b) => (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt) ? -1 : 1))
+              .map(post => (
+                <Link key={post.slug} className="flex flex-col space-y-1 mb-4" href={`/blog/${post.slug}`}>
+                  <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+                    <p className="text-muted-foreground w-[100px] tabular-nums">
+                      {formatDate(post.metadata.publishedAt, false)}
+                    </p>
+                    <p className="tracking-tight">{post.metadata.title}</p>
+                  </div>
+                </Link>
+              ))}
+          </div>
+        ))}
+      {numPosts === 0 && (
+        <p>all blogs are currently in my mind&apos;s latent space and i&apos;m struggling to get them out...</p>
+      )}
     </div>
   );
 }
