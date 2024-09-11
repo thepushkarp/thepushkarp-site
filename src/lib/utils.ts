59b0cc116fd0a461d2879b221cddd9d1b77b9916
Pushkar Patel <git@thepushkarp.com>
2024-09-12 00:50:41 +0530

    refactor(components): update post list styling and date formatting
    - Replaced `formatDate` with `formatDateYYYYMMDD` for consistent date formatting.
    - Updated post list styling to use a list element with improved hover effects and layout.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDateYYYYMMDD(date: string): string {
+  if (!date.includes('T')) {
+    date = `${date}T00:00:00`;
+  }
+  const targetDate = new Date(date);
+
+  const year = targetDate.getFullYear();
+  const month = String(targetDate.getMonth() + 1).padStart(2, '0');
+  const day = String(targetDate.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
 }
