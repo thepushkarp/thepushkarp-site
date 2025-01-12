640448d5b5d9c0d7a0dcee4000bd57e2ab7ffdbb
Pushkar Patel <git@thepushkarp.com>
2025-01-12 20:06:26 +0530

    refactor: Move loadGoogleFont to utils
    - Move the loadGoogleFont function to the utils file
    - Update the import in og route

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,20 +1,6 @@
+import { loadGoogleFont } from '@/lib/utils';
 import { ImageResponse } from 'next/og';
 import { NextRequest } from 'next/server';
-
-async function loadGoogleFont(font: string, text: string) {
-  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`;
-  const css = await (await fetch(url)).text();
-  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
-
-  if (resource) {
-    const response = await fetch(resource[1]);
-    if (response.status == 200) {
-      return await response.arrayBuffer();
-    }
-  }
-
-  throw new Error('failed to load font data');
-}
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -60,3 +60,18 @@ export function formatDateYYYYMMDD(date: string): string {
 
   return `${year}-${month}-${day}`;
 }
+
+export async function loadGoogleFont(font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+
+  if (resource) {
+    const response = await fetch(resource[1]);
+    if (response.status == 200) {
+      return await response.arrayBuffer();
+    }
+  }
+
+  throw new Error('failed to load font data');
+}
