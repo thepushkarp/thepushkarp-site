61325834b01d9bef288b86057ff8dfa768b61a50
Pushkar Patel <git@thepushkarp.com>
2025-03-27 15:23:49 +0530

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,9 +67,9 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/images/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/images/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/images/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/images/favicon.ico', sizes: '48x48', type: 'image/x-icon' },
+      { url: '/images/favicon.svg', sizes: 'any', type: 'image/svg+xml' },
     ],
     apple: [{ url: '/images/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
   },
